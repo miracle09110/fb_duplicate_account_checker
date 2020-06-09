40f53e436110b326f80a872ae3b40c314c96f5f3
Franco Mapua <franco.mapua@cleanaway.com.au>
2020-06-09 08:39:35 +0800

    Implementing eval.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const prompts = require('prompts');
 const path = require('path');
-
+const eval = require('./eval');
 const maleDefaultDP = "https://scontent.fmnl8-1.fna.fbcdn.net/v/t1.30497-1/c141.0.480.480a/p480x480/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_eui2=AeEqI6OYmIGNlw0jTefaB4tmLYw35eBBIRQtjDfl4EEhFIMk82TpqAmmtsGfzNVsal4&_nc_ohc=-PIzNlCh8LAAX-uFxfQ&_nc_ht=scontent.fmnl8-1.fna&oh=a2db7c3764f2da46d4d107221bb531ea&oe=5F015488";
 const femaleDefaultDP ="https://scontent.fmnl8-1.fna.fbcdn.net/v/t1.30497-1/c141.0.480.480a/p480x480/84688533_170842440872810_7559275468982059008_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_eui2=AeGwxdpAns8Ea9jYvh83QQznwBqKCN3Pzv_AGooI3c_O_8_3X9jOsKLqrUS0inksEOo&_nc_ohc=PgSc7iSS3nUAX95o3Ar&_nc_ht=scontent.fmnl8-1.fna&oh=2d21dcb6cf79776aa92efe5316a0dfd3&oe=5F023B58";
 
@@ -93,22 +93,16 @@ console.log(' *** Whatever happens with us, stays with us ***');
   const page = await browser.newPage();
 
   try {
-      await page.goto('https://facebook.com');
-      // await page.screenshot({ path: './screenshots/page.png' });
-      await page.waitFor('input[name=email]');
-      await page.waitFor('input[name=pass]');
-      await page.evaluate((username) => {
-        document.querySelector('input[name=email]').value = username;
-      }, username); //TODO: ADD FB USER NAME
-
-      await page.evaluate((password) => {
-        document.querySelector('input[name=pass]').value = password;
-      }, password); //TODO: ADD FB USER NAME
-
-      await page.waitFor('input[type="submit"]');
-      await page.click('input[type="submit"]');
-      await page.waitForNavigation({ waitUntil: 'networkidle2' })
-      // await page.screenshot({ path: './screenshots/login.png' });
+    await page.goto('https://facebook.com');
+    // await page.screenshot({ path: './screenshots/page.png' });
+    await page.waitFor('input[name=email]');
+    await page.waitFor('input[name=pass]');
+    await page.evaluate(eval.parseStringFunc('EVAL_USERNAME'), username); //TODO: ADD FB USER NAME
+    await page.evaluate(eval.parseStringFunc('EVAL_PASSWORD'), password); //TODO: ADD FB USER NAME
+    await page.waitFor('input[type="submit"]');
+    await page.click('input[type="submit"]');
+    await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    // await page.screenshot({ path: './screenshots/login.png' });
   }catch (err){
     console.log(`Something went wrong in logging your account`);
     await browser.close();
@@ -133,18 +127,12 @@ while (searchAgain || !rerun) {
       let iterator = 0;
       while (keepScrolling) {
         console.log('Scrolling....');
-        await page.evaluate(() => {
-          window.scrollBy(0, window.innerHeight);
-        });
+        await page.evaluate(eval.parseStringFunc('EVAL_WINDOW_SCROLL'));
 
         console.log('Loading....');
         // await page.screenshot({ path: `./screenshots/scroll${iterator}.png` });
 
-        const spanTexts = await page.evaluate(() =>
-          Array.from(document.body.querySelectorAll('span'), (el) => {
-            return el.innerText;
-          })
-        );
+        const spanTexts = await page.evaluate(eval.parseStringFunc('EVAL_GET_SPANS'));
 
         console.log(`Info gathered: ${spanTexts.length}`);
         if (spanTexts.indexOf('End of Results') > -1 || iterator === numberOfScrollIterations) {
@@ -157,11 +145,7 @@ while (searchAgain || !rerun) {
     }
   
     await scrolldown();
-    const hrefs = await page.evaluate(() =>
-      Array.from(document.body.querySelectorAll('a'), (el) =>
-        el.getAttribute('href')
-      )
-    );
+    const hrefs = await page.evaluate(eval.parseStringFunc('EVAL_GET_HREFS'));
 
     console.log(`Account Links Found ${hrefs.length}`);
     const filter = async (links) => {
@@ -185,17 +169,9 @@ while (searchAgain || !rerun) {
         console.log(`Checking ${link}...`);
         await page.goto(link);
 
-        const images = await page.evaluate(() =>
-          Array.from(document.body.querySelectorAll('img'), (el) => {
-            return el.getAttribute('src');
-          })
-      );
+        const images = await page.evaluate(eval.parseStringFunc('EVAL_GET_SOURCES'));
 
-        const texts = await page.evaluate(() =>
-           Array.from(document.body.querySelectorAll('span'), (el) => {
-             return el.innerText;
-           })
-         );
+        const texts = await page.evaluate(eval.parseStringFunc('EVAL_GET_SPANS'));
 
         if (
           images.indexOf(femaleDefaultDP) > -1 ||
